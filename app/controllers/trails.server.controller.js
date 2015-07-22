4d22dbaa3a99460682e71f4e5d8061008902e66d
Levi S. Morris <siromivel@gmail.com>
2015-07-21 18:24:59 -0600

    Added a route to handle requests for location specific data.

--- app/controllers/trails.server.controller.js
+++ app/controllers/trails.server.controller.js
@@ -3,17 +3,14 @@
 var Trail = require('mongoose').model('Trail');
 
 var featureCollection = function(contents) {
-		this.type = "FeatureCollection";
-		this.features = contents;
+	this.type = "FeatureCollection";
+	this.features = contents;
 };
 
 exports.findAll = function(req, res, next) {
 	Trail.find({}, {'_id' : 0, '__v' : 0}, function(err, trails) {
-		if (err) {
-			return next(err);
-		} else {
-			res.json(new featureCollection(trails));
-		}
+		if (err) { return next(err) }
+		res.json(new featureCollection(trails));
 	});
 };
 
@@ -21,20 +18,23 @@ exports.findByName = function(req, res, next) {
     var trailName = new RegExp(req.params.trail, "i");
 
 	Trail.find({ "properties.name" : trailName }, function(err, trail) {
-		if (err) {
-			return next(err);
-		} else {
-			res.json(trail);
-		}
+		if (err) { return next(err) }
+		res.json(trail);
 	});
 };
 
 exports.findByDifficulty = function(req, res, next) {
 	Trail.find({ "properties.difficulty" : req.params.difficulty }, {'_id' : 0, '__v' : 0}, function(err, trails) {
-		if (err) {
-			return next(err);
-		} else {
-			res.json(new featureCollection(trails));
-		}
+		if (err) { return next(err) }
+		res.json(new featureCollection(trails));
 	});
 };
+
+exports.findByLocation = function(req, res, next) {
+	var loc = new RegExp(req.params.trail, "i");
+
+	Trail.find({ "properties.park" : loc }, { "_id" : 0, "__v" : 0 }, {"sort" : "properties.parkID" }, function(err, trails) {
+		if (err) { return next(err) }
+		res.json(new featureCollection(trails));
+	});
+}
--- app/routes/trails.server.routes.js
+++ app/routes/trails.server.routes.js
@@ -6,4 +6,5 @@ module.exports = function(app) {
 	app.route(/\/trails\/?$/).get(trails.findAll);
 	app.route('/trails/:trail').get(trails.findByName);
 	app.route('/difficulty/:difficulty').get(trails.findByDifficulty);
+	app.route('/location/:loc').get(trails.findByLocation);
 };
