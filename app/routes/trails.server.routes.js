6bdd0beaea2ab5838959c4379217f778f79e8e85
siromivel <siromivel@gmail.com>
2015-08-31 13:39:34 -0400

    Testing resets

--- app/routes/trails.server.routes.js
+++ app/routes/trails.server.routes.js
@@ -3,8 +3,15 @@
 var trails = require('../controllers/trails.server.controller.js');
 
 module.exports = function(app) {
-	app.route(/\/trails\/?$/).get(trails.findAll);
-	app.route('/trails/:trail').get(trails.findByName);
-	app.route('/difficulty/:difficulty').get(trails.findByDifficulty);
-	app.route('/location/:loc').get(trails.findByLocation);
+	app.route(/\/trails\/?$/)
+		.get(trails.findAll);
+
+	app.route('/trails/:trail')
+		.get(trails.findByName);
+
+	app.route('/difficulty/:difficulty')
+		.get(trails.findByDifficulty);
+
+	app.route('/location/:loc')
+		.get(trails.findByLocation);
 };
